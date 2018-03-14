c71a9721f75bc06934afdd083ce4f1c63b0121dc
bcastillo1 <bcastillo1@vcd-ch204-23.eastern.ewu.edu>
2018-03-14 13:00:26 -0700

    Code

---
+++ js/index.js
@@ -0,0 +1,12 @@
+// Plugin: https://codepen.io/manikoth/pen/RMbomv.js
+
+console.log('Be like Browser.');
+
+// Light Gallery
+$('.gallery').lightGallery();
+// Plugin: https://codepen.io/manikoth/pen/RMbomv.js
+
+$('.img-holder').imageScroll({
+  parallax: true,
+  coverRatio: 0.5
+});
